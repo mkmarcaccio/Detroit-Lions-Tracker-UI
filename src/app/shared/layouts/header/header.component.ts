21197e7656e0ae5c54482a50b2420c77f15d0a59
mkmarcaccio <mkmarcaccio@gmail.com>
2022-07-01 13:33:53 -0400

    Add seasonid as query parameter
    Add seasonid as query parameter

--- src/app/shared/layouts/header/header.component.ts
+++ src/app/shared/layouts/header/header.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit, Output, EventEmitter, OnDestroy } from '@angular/core';
+import { forkJoin } from 'rxjs';
+import { SeasonStats } from 'src/app/models/season-stats';
+import { DetroitLionsTrackerService } from 'src/services/detroit-lions-tracker.service';
 
 @Component({
   selector: 'app-header',
@@ -7,10 +10,26 @@ import { Component, OnInit, Output, EventEmitter, OnDestroy } from '@angular/cor
 })
 export class HeaderComponent implements OnInit {
   @Output() public sideNavToggle = new EventEmitter();
+  public seasonStats: SeasonStats[];
+  public oneSeason: SeasonStats;
+  public SeasonId: number;
 
-  constructor() { }
+  constructor(
+    private detroitLionsTrackerService: DetroitLionsTrackerService
+  ) { }
 
   ngOnInit() {
+
+    const seasonsCall = this.detroitLionsTrackerService.getSeasons();
+    const requestArray = [];
+    requestArray.push(seasonsCall);
+
+    forkJoin(requestArray).subscribe(results => {
+      this.seasonStats = results[0];
+
+      this.oneSeason = this.seasonStats[0];
+      this.SeasonId = this.oneSeason.seasonId;
+    });
   }
 
   public onToggleSideNav = () => {
